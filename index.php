0746b1a9d863331cd191a661df727b1d2f754545
davidjungman <dawettoxin@seznam.cz>
2017-06-02 14:31:49 +0200

    Starting

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="source/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+        <link href="source/bootstrap/css/bootstrap.css" rel="stylesheet">
+        <link href="source/bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+        <link href="css/custom.css" rel="stylesheet">
+        <link href="css/layout.css" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
---
+++ init.php
@@ -0,0 +1,17 @@
+<?php
+
+    require_once "config/defines.mysql.php";
+    require_once("vendor/autoload.php");
+
+    $db = new Dibi\Connection([
+            'driver'   => 'mysqli',
+            'host'     => MYSQL_HOSTNAME,
+            'username' => MYSQL_USERNAME,
+            'password' => MYSQL_PASSWORD,
+            'database' => MYSQL_DATABASE
+    ]);
+     
+    require_once("functions.php");
+    
+// Everytime used class
+    $fm = new flassMessages();
